0a7f85a9fefce05091f7948d293c8a26b7a5a9fa
Mansi Odedra <odedrashanti76@gmail.com>
2025-02-26 15:16:15 +0530

    applied universal navbar
    tested and modified

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,30 +1,42 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import Dashboard from "../pages/Dashboard";
 import NotFound from "../pages/NotFound";
 import Home from "../pages/Home";
 import Tickets from "../pages/Tickets";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'; 
-
+import Navbar from "../components/Navbar"; // Import Navbar
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const AppRoutes = () => {
   return (
     <Router>
-       <ToastContainer />
+      <AuthWrapper />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/tickets" element={<Tickets />} />
-        <Route path="/home/*" element={<Home />} /> {/* Add Home route */}
+        <Route path="/home/*" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+
+      {/* Toast Notifications */}
+      <ToastContainer position="top-center" autoClose={3000} />
     </Router>
   );
+};
+
+// Wrapper to conditionally show Navbar based on localStorage
+const AuthWrapper = () => {
+  const location = useLocation();
+  const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
+  const noNavbarPaths = ["/", "/login", "/register"];
+
+  return isAuthenticated && !noNavbarPaths.includes(location.pathname) ? <Navbar /> : null;
 };
 
 export default AppRoutes;
