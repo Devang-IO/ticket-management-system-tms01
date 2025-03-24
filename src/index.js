682df0031796902bb4599aa760d48449ba79b6b1
Devang <90950290+Devang-IO@users.noreply.github.com>
2025-03-24 17:13:48 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";  // Use `client` from React 18
-import App from "./App";
-import "./styles/global.css";
-/*  
-  ┏━━━━━━━━━━━━━━━━━━━━━━┓
-  ┃  ROAR! I AM DEX! 🐉  ┃
-  ┃ PROTECTOR OF CODE! 🔥┃
-  ┗━━━━━━━━━━━━━━━━━━━━━━┛
-          \    
-           \  (╯°□°）╯︵ ┻━┻ 
-            \   /|   
-             ( (  🔥🔥🔥
-              \_)
-*/
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
